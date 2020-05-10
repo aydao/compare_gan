815b82a90cf009808cfd00958cb22f0cd6020a0e
aydao <ayompm@protonmail.com>
2020-05-10 10:47:38 -0400

    Update configs

--- ay_bgm_256_run02.sh
+++ ay_bgm_256_run02.sh
@@ -3,10 +3,10 @@ set -ex
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-/tfk/lib}"
 export TPU_HOST=${TPU_HOST:-10.255.128.3}
 export TPU_NAME="${TPU_NAME:-tpu-v3-256-euw4a-1}"
-export MODEL_DIR="${MODEL_DIR:-gs://ay1-euw4a/aytest/bgm-danbooru2019-s-256-run1/}"
+export MODEL_DIR="${MODEL_DIR:-gs://ay1-euw4a/aytest/bgm-danbooru2019-s-256-run2/}"
 export DATASETS=gs://ay1-euw4a/datasets/danbooru2019-s/danbooru2019-s-0*
 export LABELS=""
 export NUM_CLASSES=1000
 export TPU_SPLIT_COMPILE_AND_EXECUTE=1
 export TF_TPU_WATCHDOG_TIMEOUT=1800
-python3 wrapper.py compare_gan/main.py --use_tpu --tfds_data_dir 'gs://ay1-euw4a/datasets/' --model_dir "${MODEL_DIR}" --gin_config example_configs/ay_bgm_danbooru256_run01.gin "$@"
+python3 wrapper.py compare_gan/main.py --use_tpu --tfds_data_dir 'gs://ay1-euw4a/datasets/' --model_dir "${MODEL_DIR}" --gin_config example_configs/ay_bgm_danbooru256_run02.gin "$@"
