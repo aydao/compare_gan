822eeef2af804af30f1ba9a6599c707cd757df5e
aydao <ayompm@protonmail.com>
2020-06-07 00:51:29 -0400

    Update config

---
+++ ay_bgm_512_run01.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-/ayy/lib}"
+export TPU_HOST=${TPU_HOST:-10.255.128.3}
+export TPU_NAME="${TPU_NAME:-tpu-v3-256-euw4a-1}"
+export NUM_CLASSES=1000
+export TPU_SPLIT_COMPILE_AND_EXECUTE=1
+export TF_TPU_WATCHDOG_TIMEOUT=1800
+while true; do
+  python3 wrapper.py compare_gan/main.py --gin_config ./example_config/ay_bgm_danbooru512_run01.gin --use_tpu --tfds_data_dir 'gs://ay1-euw4a/datasets/' "$@"
+  sleep 30
+done
