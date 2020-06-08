695f10f0103972f82315adf268bd95d09eef593d
aydao <ayompm@protonmail.com>
2020-06-08 19:11:56 +0000

    Renaming

--- ay_bg_128_run01.sh
+++ ay_bg_128_run01.sh
@@ -7,6 +7,6 @@ export NUM_CLASSES=1000
 export TPU_SPLIT_COMPILE_AND_EXECUTE=1
 export TF_TPU_WATCHDOG_TIMEOUT=1800
 while true; do
-  timeout --signal=SIGTERM 19h python3 wrapper.py compare_gan/main.py --gin_config ./example_configs/ay_bgm_danbooru128_run03.gin --use_tpu --tfds_data_dir 'gs://ay1-euw4a/datasets/' "$@"
+  timeout --signal=SIGTERM 19h python3 wrapper.py compare_gan/main.py --gin_config ./example_configs/ay_bg_danbooru128_run01.gin --use_tpu --tfds_data_dir 'gs://ay1-euw4a/datasets/' "$@"
   sleep 30
 done
